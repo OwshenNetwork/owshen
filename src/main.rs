90d53923677b92c5ebefb7e1f5dc2b1a47702fe9
Owshen Network <owshennetwork@gmail.com>
2023-08-13 20:51:47 +0330

    Dynamically generate proofs in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod fp;
 pub use fp::Fp;
 
-use ff::Field;
+use ff::{Field, PrimeField};
 
 use bindings::counter::Counter;
+use num_bigint::BigUint;
 
 use ethers::prelude::*;
 use ethers::utils::Ganache;
 
+use ethers::abi::ethabi::ethereum_types::FromStrRadixErr;
 use eyre::Result;
 use rand::Rng;
+use std::process::Command;
 use std::sync::Arc;
 
 fn hash(left: Fp, right: Fp) -> Fp {
@@ -45,12 +48,78 @@ impl From<PrivateKey> for PublicKey {
     }
 }
 
+#[derive(Clone, Debug)]
+struct Proof {
+    a: [U256; 2],
+    b: [[U256; 2]; 2],
+    c: [U256; 2],
+    public: Vec<U256>,
+}
+
+fn prove(a: Fp, b: Fp) -> Result<Proof> {
+    std::fs::write(
+        "contracts/circuits/coin_withdraw_input.json",
+        format!(
+            "{{ \"a\": {:?}, \"b\": {:?} }}",
+            BigUint::from_bytes_le(a.to_repr().as_ref()),
+            BigUint::from_bytes_le(b.to_repr().as_ref())
+        ),
+    )?;
+    let wtns_gen_output = Command::new("contracts/circuits/coin_withdraw_cpp/coin_withdraw")
+        .arg("contracts/circuits/coin_withdraw_input.json")
+        .arg("contracts/circuits/coin_withdraw_witness.wtns")
+        .output()?;
+
+    assert_eq!(wtns_gen_output.stdout.len(), 0);
+    assert_eq!(wtns_gen_output.stderr.len(), 0);
+
+    let proof_gen_output = Command::new("snarkjs")
+        .arg("groth16")
+        .arg("prove")
+        .arg("contracts/circuits/coin_withdraw_0001.zkey")
+        .arg("contracts/circuits/coin_withdraw_witness.wtns")
+        .arg("contracts/circuits/coin_withdraw_proof.json")
+        .arg("contracts/circuits/coin_withdraw_public.json")
+        .output()?;
+
+    assert_eq!(proof_gen_output.stdout.len(), 0);
+    assert_eq!(proof_gen_output.stderr.len(), 0);
+
+    let generatecall_output = Command::new("snarkjs")
+        .arg("generatecall")
+        .arg("contracts/circuits/coin_withdraw_public.json")
+        .arg("contracts/circuits/coin_withdraw_proof.json")
+        .output()?;
+    let mut calldata = std::str::from_utf8(&generatecall_output.stdout)?.to_string();
+    calldata = calldata
+        .replace("\"", "")
+        .replace("[", "")
+        .replace("]", "")
+        .replace(" ", "")
+        .replace("\n", "");
+    let data = calldata
+        .split(",")
+        .map(|k| U256::from_str_radix(k, 16))
+        .collect::<Result<Vec<U256>, FromStrRadixErr>>()?;
+
+    let proof = Proof {
+        a: data[0..2].try_into()?,
+        b: [data[2..4].try_into()?, data[4..6].try_into()?],
+        c: data[6..8].try_into()?,
+        public: data[8..].to_vec(),
+    };
+
+    Ok(proof)
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Welcome to Owshen Client!");
 
     let sk = PrivateKey::generate(&mut rand::thread_rng());
     println!("Public key: {:?}", PublicKey::from(sk.clone()));
+
+    println!("Proof: {:?}", prove(123.into(), 234.into())?);
 
     let port = 8545u16;
     let url = format!("http://localhost:{}", port).to_string();
