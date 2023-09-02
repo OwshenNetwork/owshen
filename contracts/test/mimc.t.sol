4e67f4d178a1f1796dbbe8a958a08db0410e9062
Owshen Network <owshennetwork@gmail.com>
2023-09-02 13:51:51 +0330

    Add MiMC solidity implementation

---
+++ contracts/test/mimc.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/mimc.sol";
+
+contract MiMCTest is Test {
+    MiMC public mimc;
+
+    function setUp() public {
+        mimc = new MiMC();
+    }
+
+    function testMiMC() public {
+        assertEq(mimc.hashLeftRight(3, 11), 20873465551905417246270225393360073881989948543683254892256709153974136274798);
+    }
+}
